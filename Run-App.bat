4fb8e3ebf270f201b03102b28b8619a76ef77db8
Meghamsh <megha@bio172037.college.tcd.ie>
2025-11-30 14:48:36 +0000

    Launcher: use fuser kill; ignore local venv

--- Run-App.bat
+++ Run-App.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 for /f "usebackq tokens=* delims=" %%i in (`wsl wslpath "%~dp0"`) do set WSL_DIR=%%i
-wsl -e bash -lc "netstat -tlnp | grep -E ':(5174|8002) ' | awk '{print $7}' | cut -d/ -f1 | xargs -r kill -9"
+wsl -e bash -lc "fuser -k 5174/tcp 8002/tcp" >nul 2>&1
 wsl -e bash -lc "cd '%WSL_DIR%modern-app' && npm run dev:full"
 timeout /t 4 >nul
 start "" http://localhost:5174
